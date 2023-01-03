c2e79a6ebacfb7a9ea11853ed7f2fe425ce8a5c8
sheeeetal <sviradiya148@gmail.com>
2023-01-03 09:17:57 -1200

    My Homework Week18

---
+++ src/test/java/com/bestbuy/basetest/TestBase.java
@@ -0,0 +1,17 @@
+package com.bestbuy.basetest;
+
+import com.bestbuy.utils.PropertyReader;
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+public class TestBase {
+    public static PropertyReader propertyReader;
+
+    @BeforeClass
+    public static void init() {
+        propertyReader = PropertyReader.getInstance();
+        RestAssured.baseURI = propertyReader.getProperty("baseUrl");
+        RestAssured.port = Integer.parseInt(propertyReader.getProperty("port"));
+        //RestAssured.basePath = Path.STORES;
+    }
+}
